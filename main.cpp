c54aba59f6fb2ee2aae578dc10f38fb71ac7a6b9
wufeiyun <wufeiyun@emindsoft.com.cn>
2017-12-15 12:30:26 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <qglobal.h>
+#if QT_VERSION >= 0x050000
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#else
+#endif
+
+int main(int argc, char *argv[])
+{
+#if defined(Q_OS_WIN)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
